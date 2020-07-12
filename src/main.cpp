b4d0f054190ede0f793fac3610e94b05f11f6726
vaibhav1284 <vaibhav1284@gmail.com>
2020-07-12 16:04:58 +0530

    changes in main func

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <fstream>
 #include <math.h>
 #include <uWS/uWS.h>
-#include <chrono>
+//#include <chrono>
 #include <iostream>
-#include <thread>
+//#include <thread>
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
